6eb7d90b8e9ad5c69c4f6dee9612790f55c74bca
Herman Martinus <hfbmartinus@gmail.com>
2021-11-09 12:02:47 +0200

    Testing the proxy live

--- proxy/urls.py
+++ proxy/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path
 from django.views.generic.base import TemplateView
 
@@ -5,4 +6,5 @@ from .views import *
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='base.html'), name='home'),
+    url(r'^proxy/(?P<url>.*)$', reverse_proxy, name='proxy'),
 ]
--- proxy/views.py
+++ proxy/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import Http404, HttpResponse
 
-# Create your views here.
+import requests
+
+
+@csrf_exempt
+def reverse_proxy(request, url):
+    requestor = getattr(requests, request.method.lower())
+    
+    proxied_response = requestor(url, data=request.body, files=request.FILES)
+    
+    response = HttpResponse(proxied_response.content, content_type=proxied_response.headers.get('content-type'))
+    for header_key, header_value in proxied_response.headers.items():
+        response[header_key] = header_value
+    
+    response.headers["Access-Control-Allow-Origin"] = '*'
+    response.headers["Vary"] = "Origin"
+    return response

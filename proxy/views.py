40d8583fad041a84df90718b0c2f51c0e6223890
Herman Martinus <hfbmartinus@gmail.com>
2021-11-09 12:43:47 +0200

    Ignore hop-by-hop headers

--- proxy/views.py
+++ proxy/views.py
@@ -8,12 +8,13 @@ import requests
 @csrf_exempt
 def reverse_proxy(request, url):
     requestor = getattr(requests, request.method.lower())
-    
+
     proxied_response = requestor(url, data=request.body, files=request.FILES)
     
     response = HttpResponse(proxied_response.content, content_type=proxied_response.headers.get('content-type'))
     for header_key, header_value in proxied_response.headers.items():
-        response.headers[header_key] = header_value
+        if header_key not in 'ConnectionKeep-AliveProxy-AuthenticateProxy-AuthorizationTETrailersTransfer-EncodingUpgrade':
+            response.headers[header_key] = header_value
     
     response.headers["Access-Control-Allow-Origin"] = '*'
     response.headers["Vary"] = "Origin"

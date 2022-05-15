ad94d8633808ff699f830b37d6538eb6eb404f08
Herman Martinus <hfbmartinus@gmail.com>
2022-05-15 21:04:27 +0800

    test: using sqlite

--- proxy/middleware.py
+++ proxy/middleware.py
@@ -2,5 +2,4 @@ class AddCORSHeaders(object):
     def process_response(self, proxy, request, upstream_response, response):
         response.headers['Access-Control-Allow-Origin'] = "*"
         response.headers["Vary"] = "Origin"
-
         return response
